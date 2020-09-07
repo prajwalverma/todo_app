e99768f7670a0046d9f716b29dcbfe793cce658f
prajwalverma <59706696+prajwalverma@users.noreply.github.com>
2020-09-07 19:19:04 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <ToDoList />
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/ToDoList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ToDoList
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url('https://use.fontawesome.com/releases/v5.7.2/css/all.css');
+  
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    background: url("todo.jpg");
+  }
+
+  #app {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    color: #333;
+    width: 100%;
+    max-width: 768px;
+  }
+</style>
